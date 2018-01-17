ea2de244e6579b1bcba4305250ed79232d874fa7
SanchesQA <Sasha388951>
2018-01-17 16:19:31 +0100

    refactoring

---
+++ src/test/java/conf/BasePage.java
@@ -0,0 +1,13 @@
+package conf;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+  protected WebDriver driver;
+
+  public Driver (WebDriver driver){
+    this.driver = driver;
+  }
+
+}
--- src/test/java/conf/Driver.java
+++
@@ -1,9 +0,0 @@
-package conf;
-
-
-import org.openqa.selenium.WebDriver;
-
-public class Driver {
-  public WebDriver driver;
-
-}
